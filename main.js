1e78d54330305c7daa20d4957a6c241f6d589fe1
lifeng-yin <lifeng-yin@proton.me>
2023-06-02 15:02:21 -0400

    add more skill icons

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import './style.css'
+import Typed from 'typed.js';
+
+
+const typed = new Typed('#typing-subtitle', {
+    stringsElement: '#typing-strings',
+    typeSpeed: 50,
+    loop: true,
+    startDelay: 50,
+    typeSpeed: 75,
+    backDelay: 2000,
+    backSpeed: 35
+});
